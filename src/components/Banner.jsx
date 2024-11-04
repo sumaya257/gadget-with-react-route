8da6d75db639ffebaac27efd07816b3b142a6d6f
sumaya257 <sumayaece19@gmai.com>
2024-11-04 21:43:43 +0600

    banner-part

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
             <div className="text-center h-[594px] bg-[#9538E2] text-white p-10 rounded-2xl border-2">
                 <h1 className="text-5xl font-bold my-8">Upgrade Your Tech Accessorize with <br />Gadget Heaven Accessories</h1>
                 <p className="mt-2">Explore the latest gadgets that will take your experience to the next level. From smart devices to <br /> the coolest accessories, we have it all!</p>
-                <Link to='/dashboard' className='btn bg-white text-[#9538E2] rounded-2xl mt-8'>Shop Now</Link>
+                <Link to='/dashboard' className='btn bg-white text-[#9538E2] rounded-2xl mt-8 px-5'>Shop Now</Link>
             </div>
 
             {/* Image Div */}
